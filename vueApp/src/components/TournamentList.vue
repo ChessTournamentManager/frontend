12278062ce038afbecfe63abd8362dceb4e4bb71
Judah <i460958@fhict.nl>
2023-01-09 14:46:05 +0100

    Tournament titles are put on the main page

---
+++ vueApp/src/components/TournamentList.vue
@@ -0,0 +1,44 @@
+<template>
+    <di>
+        <h1>Tournaments</h1>
+        <ul>
+            <li v-for="tournament of tournaments" :key="tournament.id">
+                {{ tournament.name }}
+            </li>
+        </ul>
+    </di>
+</template>
+  
+<script>
+  import axios from "axios";
+  
+  export default {
+    name: "TournamentList",
+    data() {
+      return {
+        tournaments: []
+      };
+    },
+    async created() {
+      try {
+        const res = await axios.get(`http://localhost:8080/tournament`);  
+        console.log(res);
+        console.log(res.data);
+        this.tournaments = res.data;
+        console.log(this.tournaments);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  };
+</script>
+  
+<style scoped>
+  h1 {
+    text-decoration: underline;
+  }
+  
+  li {
+    color: white;
+  }
+</style>
--- vueApp/src/views/Main.vue
+++ vueApp/src/views/Main.vue
@@ -1,12 +1,17 @@
 <template>
     <NavigationBar />
     <h2>Main Page</h2>
+    <TournamentList/>
 </template>
 
 <script>
     import NavigationBar from "@/components/NavigationBar.vue";
+    import TournamentList from "@/components/TournamentList.vue";
 
     export default {
-        components: { NavigationBar }
+        components: { 
+            NavigationBar,
+            TournamentList
+        }
     };
 </script>
